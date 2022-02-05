7661e5f438adffce00e0c50f50e6c51483c0b3ef
Okimii <92546867+Okimii@users.noreply.github.com>
2022-02-05 13:26:28 -0400

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,23 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'akeno'
+copyright = '2022, Okimii'
+author = 'Okimii'
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.extlinks",
+]
+
+templates_path = ['_templates']
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+html_theme = 'alabaster'
+
+html_static_path = ['_static']
