9f11f28dd33f03717bea66ea1e0b5514bbf62b4a
Okimii <92546867+Okimii@users.noreply.github.com>
2022-02-05 23:11:26 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,6 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-html_theme = 'groundwork'
+html_theme = 'sphinx_material'
 
 html_static_path = ['_static']
