aa904b4132664371e5ae034a27d6355e50f2ab42
Okimii <92546867+Okimii@users.noreply.github.com>
2022-02-07 19:48:25 -0400

    fixed typehints

--- akeno/client.py
+++ akeno/client.py
@@ -9,7 +9,6 @@ __all__ = ("AkenoClient",)
 class AkenoClient:
     """
     A client class that has methods to access the twitter API.
-
     Parameters
     ----------
     token: :class:`str` Bare token used for authorization
@@ -49,11 +48,9 @@ class AkenoClient:
     async def fetch_tweet(self, tweet_id: int) -> dict[Any, Any]:
         """
         Makes a request to the api to get a tweet.
-
         Parameters
         ----------
         tweet_id: :class:`int` id of the tweet you're trying to fetch.
-
         Returns
         -------
         :class:`dict`
@@ -68,11 +65,9 @@ class AkenoClient:
     def get_tweet(self, tweet_id: int) -> dict[Any, Any]:
         """
         Gets the tweet by id from cache.
-
         Parameters
         ----------
         tweet_id: :class:`int` id of the tweet you're trying to get.
-
         Returns
         -------
         :class:`dict`
@@ -82,11 +77,9 @@ class AkenoClient:
     async def getch_tweet(self, tweet_id: int) -> dict[Any, Any]:
         """
         Tries to get the tweet by id from cache, if it fails it will make a request to the api.
-
         Parameters
         ----------
         tweet_id: :class:`int` id of the tweet you're trying to get or fetch.
-
         Returns
         -------
         :class:`dict`
@@ -103,14 +96,12 @@ class AkenoClient:
             self.cache[tweet_id] = tweet
             return tweet
 
-    async def fetch_tweets(self, *tweet_ids: int) -> dict[Any, Any]:
+    async def fetch_tweets(self, *tweet_ids: tuple[int]) -> dict[Any, Any]:
         """
         Makes a request to the api to get the tweets.
-
         Parameters
         ----------
         tweet_ids: :class:`int` ids of the tweet you're trying to fetch.
-
         Returns
         -------
         :class:`dict`
@@ -123,14 +114,12 @@ class AkenoClient:
         self.cache[tweet_ids] = tweets
         return tweets
 
-    async def getch_tweets(self, *tweet_ids: int) -> dict[Any, Any]:
+    async def getch_tweets(self, *tweet_ids: tuple[int]) -> dict[Any, Any]:
         """
         Tries to get the tweets by id from cache, if it fails it will make a request to the api.
-
         Parameters
         ----------
         tweet_ids: :class:`int` ids of the tweets you're trying to get or fetch.
-
         Returns
         -------
         :class:`dict`
@@ -146,14 +135,12 @@ class AkenoClient:
             self.cache[tweet_ids] = tweets
             return tweets
 
-    def get_tweets(self, *tweet_ids: int) -> dict[Any, Any]:
+    def get_tweets(self, *tweet_ids: tuple[int]) -> dict[Any, Any]:
         """
         Gets the tweet by id from cache.
-
         Parameters
         ----------
         tweet_ids: :class:`int` ids of the tweet you're trying to get.
-
         Returns
         -------
         :class:`dict`
@@ -163,12 +150,10 @@ class AkenoClient:
     async def like_tweet(self, user_id: int, tweet_id: int) -> dict[Any, Any]:
         """
         Likes a tweet for a user.
-
         Parameters
         ----------
         user_id: :class:`int` your own id to like the given tweet.
         tweet_id: :class:`int` id of the tweets you're trying to like.
-
         Returns
         -------
         :class:`dict`
@@ -186,12 +171,10 @@ class AkenoClient:
     async def unlike_tweet(self, user_id: int, tweet_id: int) -> dict[Any, Any]:
         """
         Unlikes a tweet for a user.
-
         Parameters
         ----------
         user_id: :class:`int` your own id to like the given tweet.
         tweet_id: :class:`int` id of the tweets you're trying to like.
-
         Returns
         -------
         :class:`dict`
@@ -205,11 +188,9 @@ class AkenoClient:
     async def fetch_user(self, user_id: int) -> dict[Any, Any]:
         """
         Makes a request to the api to get a user.
-
         Parameters
         ----------
         user_id: :class:`int` id of the user you're trying to fetch.
-
         Returns
         -------
         :class:`dict`
@@ -223,11 +204,9 @@ class AkenoClient:
     def get_user(self, user_id: int) -> dict[Any, Any]:
         """
         Gets the user by id from cache.
-
         Parameters
         ----------
         user_id: :class:`int` id of the user you're trying to get.
-
         Returns
         -------
         :class:`dict`
@@ -237,11 +216,9 @@ class AkenoClient:
     async def getch_user(self, user_id: int) -> dict[Any, Any]:
         """
         Tries to get the user by id from cache, if it fails it will make a request to the api.
-
         Parameters
         ----------
         user_id :class:`int` id of the user you're trying to get or fetch.
-
         Returns
         -------
         :class:`dict`
@@ -260,11 +237,9 @@ class AkenoClient:
     async def fetch_user_profile_image(self, user_id: int) -> str:
         """
         Makes a request to the api to get a users profile picture as a url.
-
         Parameters
         ----------
         user_id: :class:`int` id of the user you're trying to fetch.
-
         Returns
         -------
         :class:`str`
@@ -280,11 +255,9 @@ class AkenoClient:
     def get_user_profile_image(self, user_id: int) -> str:
         """
         Gets the users profile picture as a url by id from cache.
-
         Parameters
         ----------
         user_id: :class:`int` id of the user you're trying to get.
-
         Returns
         -------
         :class:`str`
@@ -294,11 +267,9 @@ class AkenoClient:
     async def getch_user_profile_image(self, user_id: int) -> str:
         """
         Tries to get the user profile image by id from cache, if it fails it will make a request to the api.
-
         Parameters
         ----------
         user_id: :class:`int` id of the user you're trying to get or fetch.
-
         Returns
         -------
         :class:`str`
@@ -317,11 +288,9 @@ class AkenoClient:
     async def fetch_user_created_at(self, user_id: int) -> str:
         """
         Makes a request to the api to get a users date of the account creation.
-
         Parameters
         ----------
         user_id: :class:`int` id of the user you're trying to fetch.
-
         Returns
         -------
         :class:`str`
@@ -337,11 +306,9 @@ class AkenoClient:
     def get_user_created_at(self, user_id: int) -> str:
         """
         Gets the users date of account creation by id from cache.
-
         Parameters
         ----------
         user_id: :class:`int` id of the user you're trying to get.
-
         Returns
         -------
         :class:`str`
@@ -351,11 +318,9 @@ class AkenoClient:
     async def getch_user_created_at(self, user_id: int) -> str:
         """
         Tries to get the users date of account creation, if it fails it will make a request to the api.
-
         Parameters
         ----------
         user_id: :class:`int` id of the user you're trying to get or fetch.
-
         Returns
         -------
         :class:`str`
@@ -374,11 +339,9 @@ class AkenoClient:
     async def fetch_user_metrics(self, user_id: int) -> dict[Any, Any]:
         """
         Makes a request to the api to get the metrics of a user.
-
         Parameters
         ----------
         user_id: :class:`int` id of the user you're trying to fetch.
-
         Returns
         -------
         :class:`dict`
@@ -394,11 +357,9 @@ class AkenoClient:
     def get_user_metrics(self, user_id: int) -> dict[Any, Any]:
         """
         Gets the users metrics by id from cache.
-
         Parameters
         ----------
         user_id: :class:`int` id of the user you're trying to get.
-
         Returns
         -------
         :class:`dict`
@@ -408,7 +369,6 @@ class AkenoClient:
     async def getch_user_metrics(self, user_id: int) -> dict[Any, Any]:
         """
         Tries to get the metrics of a user from cache, if it fails it will make a request to the api.
-
         Parameters
         ----------
         user_id: :class:`int` id of the user you're trying to get or fetch.
