970830368ea3c4420fbddb50532705b20f9084ad
Okimii <92546867+Okimii@users.noreply.github.com>
2022-02-09 00:54:08 -0400

    Update httpclient.py

--- akeno/httpclient.py
+++ akeno/httpclient.py
@@ -8,53 +8,23 @@ __all__ = ("HttpClient",)
 
 
 class HttpClient:
-    """
-    A httpclient class that has methods to access the twitter API.
-
-    Parameters
-    ----------
-    token: :class:`str` Bare token used for authorization.
-    """
-
     def __init__(self, token: str) -> None:
         self.token = token
         self.lock = asyncio.Lock()
         self.headers = {"Authorization": f"Bearer {self.token}"}
 
     async def __aenter__(self) -> "HttpClient":
-        """
-        Enter for context managers.
-        """
         return self
 
     async def __aexit__(self, *_: Any) -> None:
-        """
-        Exit for context managers.
-        """
         await self.close()
 
     async def close(self) -> None:
-        """
-        Close the clientsession.
-        """
         await self.session.close()
 
     async def request(
         self, method: str, endpoint: str, headers: dict
     ) -> dict[Any, Any]:
-        """
-        A request method to access the twitter API.
-
-        Parameters
-        ----------
-        method: :class:`str` the request method.
-        endpoint: :class:`str` the endpoint url.
-        headers: :class:`dict` the request headers.
-
-        Returns
-        -------
-        :class:`dict`
-        """
         self.session = ClientSession(headers=headers)
         resp = await self.session.request(method, endpoint)
         try:
