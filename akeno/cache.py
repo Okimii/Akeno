af42a75b79e04a25999e22db94b26fc169150598
Okimii <92546867+Okimii@users.noreply.github.com>
2022-02-20 01:58:38 -0400

    Update cache.py

--- akeno/cache.py
+++ akeno/cache.py
@@ -1,26 +1,26 @@
-from typing import Any, Dict, Optional
+from typing import Any, Optional
 
 __all__ = ("Cache",)
 
 
 class Cache:
     def __init__(self):
-        self.cache: Dict[Any, Dict[Any, Any]] = {}
+        self.cache: dict[Any, dict[Any, Any]] = {}
 
-    def __dict__(self) -> Dict[Any, Any]:
+    def __dict__(self) -> dict[Any, Any]:
         return self.cache
 
     def __len__(self) -> int:
         return len(self.cache)
 
-    def save(self, key: Any, value: Dict[Any, Any]) -> None:
+    def save(self, key: Any, value: dict[Any, Any]) -> None:
         self.cache[key] = value
 
-    def get(self, key: int) -> Optional[Dict[Any, Any]]:
+    def get(self, key: int) -> Optional[dict[Any, Any]]:
         return self.cache.get(key)
 
     @property
-    def items(self) -> Optional[Dict[Any, Dict[Any, Any]]]:
+    def items(self) -> Optional[dict[Any, Dict[Any, Any]]]:
         return self.cache
 
     def get_index(self, key: int, index0: str, index1: str) -> Optional[str]:
