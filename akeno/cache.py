d298d4b0cb2da99c27a0c9398af114dd5b498ba8
Okimii <92546867+Okimii@users.noreply.github.com>
2022-02-20 01:29:35 -0400

    Update cache.py

--- akeno/cache.py
+++ akeno/cache.py
@@ -7,10 +7,10 @@ class Cache:
     def __init__(self):
         self.cache: dict[Any, dict[Any, Any]] = {}
 
-    def save(self, key: Any, value: dict[Any, Any]) -> None:
+    def _save(self, key: Any, value: dict[Any, Any]) -> None:
         self.cache[key] = value
 
-    def get(self, key: int) -> Optional[dict[Any, Any]]:
+    def _get(self, key: int) -> Optional[dict[Any, Any]]:
         try:
             return self.cache[key]
         except KeyError:
@@ -20,7 +20,7 @@ class Cache:
     def all_cache_items(self) -> Optional[dict[Any, dict[Any, Any]]]:
         return self.cache
 
-    def get_index(self, key: int, index0: str, index1: str) -> Optional[str]:
+    def _get_index(self, key: int, index: str) -> Optional[str]:
         try:
             return self.cache[key][index0][index1]
         except KeyError:
