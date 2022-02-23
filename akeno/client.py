772b0b044a541ec588d95f7f068d0f56010f0921
Okimii <92546867+Okimii@users.noreply.github.com>
2022-02-23 16:06:15 -0400

    Update client.py

--- akeno/client.py
+++ akeno/client.py
@@ -1,13 +1,14 @@
 from typing import Any
 
+from .user import User
+from .tweet import Tweet
 from .http import HTTPClient
-from .cache import Cache
-
 
 __all__ = ("AkenoClient",)
 
 
-class AkenoClient(HTTPClient, Cache):
+class AkenoClient(HTTPClient):
+
     """
     A main client class.
 
@@ -23,6 +24,7 @@ class AkenoClient(HTTPClient, Cache):
         setattr(HTTPClient, "token", token)
 
     async def fetch_tweet(self, tweet_id: int) -> dict[Any, Any]:
+
         """
         Makes a request to the api to get a tweet.
 
@@ -32,111 +34,13 @@ class AkenoClient(HTTPClient, Cache):
 
         Returns
         -------
-        :class:`dict`
+        :class:`Tweet`
         """
 
-        tweet = await self._request(
-            "GET", f"https://api.twitter.com/2/tweets/{tweet_id}", headers=self.headers
-        )
-        self._save(tweet_id, tweet)
-        return tweet
-
-    def get_tweet(self, tweet_id: int) -> dict[Any, Any]:
-        """
-        Gets the tweet by id from cache.
-
-        Parameters
-        ----------
-        tweet_id: :class:`int` id of the tweet you're trying to get.
-
-        Returns
-        -------
-        :class:`dict`
-        """
-        return self._get(tweet_id)
-
-    async def getch_tweet(self, tweet_id: int) -> dict[Any, Any]:
-        """
-        Tries to get the tweet by id from cache, if it fails it will make a request to the api.
-
-        Parameters
-        ----------
-        tweet_id: :class:`int` id of the tweet you're trying to get or fetch.
-
-        Returns
-        -------
-        :class:`dict`
-        """
-
-        try:
-            return self._get(tweet_id)
-        except KeyError:
-            tweet = await self._request(
-                "GET",
-                f"https://api.twitter.com/2/tweets/{tweet_id}",
-                headers=self.headers,
-            )
-            self._save(tweet_id, tweet)
-            return tweet
-
-    async def fetch_tweets(self, *tweet_ids: int) -> dict[Any, Any]:
-        """
-        Makes a request to the api to get the tweets.
-
-        Parameters
-        ----------
-        tweet_ids: :class:`int` ids of the tweet you're trying to fetch.
-
-        Returns
-        -------
-        :class:`dict`
-        """
-        tweets = await self._request(
-            "GET",
-            f"https://api.twitter.com/2/tweets?ids={','.join(list(map(str, [i for i in tweet_ids])))}",
-            headers=self.headers,
-        )
-        self._save(tweet_ids, tweets)
-        return tweets
-
-    async def getch_tweets(self, *tweet_ids: int) -> dict[Any, Any]:
-        """
-        Tries to get the tweets by id from cache, if it fails it will make a request to the api.
-
-        Parameters
-        ----------
-        tweet_ids: :class:`int` ids of the tweets you're trying to get or fetch.
-
-        Returns
-        -------
-        :class:`dict`
-        """
-        try:
-            return self._get(tweet_ids)
-        except KeyError:
-            tweets = await self._request(
-                "GET",
-                f"https://api.twitter.com/2/tweets?ids={','.join(list(map(str, [i for i in tweet_ids])))}",
-                headers=self.headers,
-            )
-            self._save(tweet_ids, tweets)
-            return tweets
-
-    def get_tweets(self, *tweet_ids: int) -> dict[Any, Any]:
-        """
-        Gets the tweet by id from cache.
-
-        Parameters
-        ----------
-        tweet_ids: :class:`int` ids of the tweet you're trying to get.
-
-        Returns
-        -------
-        :class:`dict`
-        """
-        return self._get(tweet_ids)
+        return await Tweet.create(tweet_id)
 
     async def like_tweet(self, user_id: int, tweet_id: int) -> dict[Any, Any]:
+
         """
         Likes a tweet for a user.
 
@@ -160,6 +64,7 @@ class AkenoClient(HTTPClient, Cache):
         )
 
     async def unlike_tweet(self, user_id: int, tweet_id: int) -> dict[Any, Any]:
+
         """
         Unlikes a tweet for a user.
 
@@ -179,6 +84,7 @@ class AkenoClient(HTTPClient, Cache):
         )
 
     async def fetch_user(self, user_id: int) -> dict[Any, Any]:
+
         """
         Makes a request to the api to get a user.
 
@@ -188,223 +94,12 @@ class AkenoClient(HTTPClient, Cache):
 
         Returns
         -------
-        :class:`dict`
+        :class:`User`
         """
-        user = await self._request(
-            "GET", f"https://api.twitter.com/2/users/{user_id}", headers=self.headers
-        )
-
-        self._save(user_id, user)
-        return user
-
-    def get_user(self, user_id: int) -> dict[Any, Any]:
-        """
-        Gets the user by id from cache.
-
-        Parameters
-        ----------
-        user_id: :class:`int` id of the user you're trying to get.
-
-        Returns
-        -------
-        :class:`dict`
-        """
-        return self._get(user_id)
-
-    async def getch_user(self, user_id: int) -> dict[Any, Any]:
-        """
-        Tries to get the user by id from cache, if it fails it will make a request to the api.
-
-        Parameters
-        ----------
-        user_id :class:`int` id of the user you're trying to get or fetch.
-
-        Returns
-        -------
-        :class:`dict`
-        """
-        try:
-            return self._get(user_id)
-        except KeyError:
-            user = await self._request(
-                "GET",
-                f"https://api.twitter.com/2/users/{user_id}",
-                headers=self.headers,
-            )
-            self._save(user_id, user)
-            return user
-
-    async def fetch_user_profile_image(self, user_id: int) -> str:
-        """
-        Makes a request to the api to get a users profile picture as a url.
-
-        Parameters
-        ----------
-        user_id: :class:`int` id of the user you're trying to fetch.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        user = await self._request(
-            "GET",
-            f"https://api.twitter.com/2/users/{user_id}?user.fields=profile_image_url",
-            headers=self.headers,
-        )
-        self._save(user_id, user)
-        return user["data"]["profile_image_url"]
-
-    def get_user_profile_image(self, user_id: int) -> str:
-        """
-        Gets the users profile picture as a url by id from cache.
-
-        Parameters
-        ----------
-        user_id: :class:`int` id of the user you're trying to get.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        return self._get_index(user_id, "data", "profile_image_url")
-
-    async def getch_user_profile_image(self, user_id: int) -> str:
-        """
-        Tries to get the user profile image by id from cache, if it fails it will make a request to the api.
-
-        Parameters
-        ----------
-        user_id: :class:`int` id of the user you're trying to get or fetch.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        try:
-            return self._get_index(user_id, "data", "profile_image_url")
-        except KeyError:
-            user = await self._request(
-                "GET",
-                f"https://api.twitter.com/2/users/{user_id}?user.fields=profile_image_url",
-                headers=self.headers,
-            )
-            self._save(user_id, user)
-            return user["data"]["profile_image_url"]
-
-    async def fetch_user_created_at(self, user_id: int) -> str:
-        """
-        Makes a request to the api to get a users date of the account creation.
-
-        Parameters
-        ----------
-        user_id: :class:`int` id of the user you're trying to fetch.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        user = await self._request(
-            "GET",
-            f"https://api.twitter.com/2/users/{user_id}?user.fields=created_at",
-            headers=self.headers,
-        )
-        self._save(user_id, user)
-        return user["data"]["created_at"]
-
-    def get_user_created_at(self, user_id: int) -> str:
-        """
-        Gets the users date of account creation by id from cache.
-
-        Parameters
-        ----------
-        user_id: :class:`int` id of the user you're trying to get.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        return self._get_index(user_id, "data", "created_at")
-
-    async def getch_user_created_at(self, user_id: int) -> str:
-        """
-        Tries to get the users date of account creation, if it fails it will make a request to the api.
-        Parameters
-        ----------
-        user_id: :class:`int` id of the user you're trying to get or fetch.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        try:
-            return self._get_index(user_id, "data", "created_at")
-        except KeyError:
-            user = await self._request(
-                "GET",
-                f"https://api.twitter.com/2/users/{user_id}?user.fields=created_at",
-                headers=self.headers,
-            )
-            self._save(user_id, user)
-            return user["data"]["created_at"]
-
-    async def fetch_user_metrics(self, user_id: int) -> str:
-        """
-        Makes a request to the api to get the metrics of a user.
-
-        Parameters
-        ----------
-        user_id: :class:`int` id of the user you're trying to fetch.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        user = await self._request(
-            "GET",
-            f"https://api.twitter.com/2/users/{user_id}?user.fields=public_metrics",
-            headers=self.headers,
-        )
-
-        return user["data"]["public_metrics"]
-
-    def get_user_metrics(self, user_id: int) -> str:
-        """
-        Gets the users metrics by id from cache.
-
-        Parameters
-        ----------
-        user_id: :class:`int` id of the user you're trying to get.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        return self._get_index(user_id, "data", "public_metrics")
-
-    async def getch_user_metrics(self, user_id: int) -> str:
-        """
-        Tries to get the metrics of a user from cache, if it fails it will make a request to the api.
-
-        Parameters
-        ----------
-        user_id: :class:`int` id of the user you're trying to get or fetch.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        try:
-            return self._get_index(user_id, "data", "public_metrics")
-        except KeyError:
-            user = await self.request(
-                "GET",
-                f"https://api.twitter.com/2/users/{user_id}?user.fields=public_metrics",
-                headers=self.headers,
-            )
-            self._save(user_id, user)
-            return user["data"]["public_metrics"]
+        return await User.create(user_id)
 
     async def retweet(self, user_id: int, tweet_id: int) -> dict[Any, Any]:
+
         """
         Makes a request to the api to retweet the given tweet.
 
@@ -428,6 +123,7 @@ class AkenoClient(HTTPClient, Cache):
         )
 
     async def post_tweet(self, text: str) -> dict[Any, Any]:
+
         """
         Makes a request to the api to post a tweet.
 
@@ -451,6 +147,7 @@ class AkenoClient(HTTPClient, Cache):
         )
 
     async def delete_tweet(self, tweet_id: int) -> dict[Any, Any]:
+        
         """
         Makes a request to the api to delete a tweet.
 
@@ -468,174 +165,3 @@ class AkenoClient(HTTPClient, Cache):
             f"https://api.twitter.com/2/tweets/{tweet_id}",
             headers=self.headers,
         )
-
-    async def fetch_tweet_created_at(self, tweet_id: int) -> str:
-        """
-        Makes a request to the api to get when the tweet was created.
-
-        Parameters
-        ----------
-        tweet_id: :class:`int` id of the tweet you're trying to fetch.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        tweet = await self._request(
-            "GET",
-            f"https://api.twitter.com/2/tweets/{tweet_id}?tweet.fields=created_at",
-            headers=self.headers,
-        )
-        self._save(tweet_id, tweet)
-        return tweet["data"]["created_at"]
-
-    def get_tweet_created_at(self, tweet_id: int) -> str:
-        """
-        Gets when the tweet was created from cache.
-
-        Parameters
-        ----------
-        tweet_id: :class:`int` id of the user you're trying to get.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        return self._get_index(tweet_id, "data", "created_at")
-
-    async def getch_tweet_created_at(self, tweet_id: int) -> str:
-        """
-        Tries to get the tweet date of creation, if it fails it will make a request to the api.
-
-        Parameters
-        ----------
-        tweet_id: :class:`int` id of the user you're trying to get or fetch.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        try:
-            return self._get_index(tweet_id, "data", "created_at")
-        except KeyError:
-            tweet = await self._request(
-                "GET",
-                f"https://api.twitter.com/2/tweets/{tweet_id}?user.fields=created_at",
-                headers=self.headers,
-            )
-            self._save(tweet_id, tweet)
-            return tweet["data"]["created_at"]
-
-    async def fetch_tweet_metrics(self, tweet_id: int) -> str:
-        """
-        Makes a request to the api to get the metrics of a tweet.
-
-        Parameters
-        ----------
-        tweet_id: :class:`int` id of the tweet you're trying to fetch.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        tweet = await self._request(
-            "GET",
-            f"https://api.twitter.com/2/tweets/{tweet_id}?tweet.fields=public_metrics",
-            headers=self.headers,
-        )
-        self._save(tweet_id, tweet)
-        return tweet["data"]["public_metrics"]
-
-    def get_tweet_metrics(self, tweet_id: int) -> str:
-        """
-        Gets the tweet metrics by id from cache.
-
-        Parameters
-        ----------
-        tweet_id: :class:`int` id of the tweet you're trying to get.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        return self._get_index(tweet_id, "data", "public_metrics")
-
-    async def getch_tweet_metrics(self, tweet_id: int) -> str:
-        """
-        Tries to get the metrics of a tweet from cache, if it fails it will make a request to the api.
-
-        Parameters
-        ----------
-        tweet_id: :class:`int` id of the tweet you're trying to get or fetch.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        try:
-            return self._get_index(tweet_id, "data", "public_metrics")
-        except KeyError:
-            tweet = await self._request(
-                "GET",
-                f"https://api.twitter.com/2/tweets/{tweet_id}?tweet.fields=public_metrics",
-                headers=self.headers,
-            )
-            self._save(tweet_id, tweet)
-            return tweet["data"]["public_metrics"]
-
-    async def fetch_tweet_source(self, tweet_id: int) -> str:
-        """
-        Makes a request to the api to get the source of a tweet.
-
-        Parameters
-        ----------
-        tweet_id: :class:`int` id of the tweet you're trying to fetch.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        tweet = await self._request(
-            "GET",
-            f"https://api.twitter.com/2/tweets/{tweet_id}?tweet.fields=source",
-            headers=self.headers,
-        )
-        self._save(tweet_id, tweet)
-        return tweet["data"]["source"]
-
-    def get_tweet_source(self, tweet_id: int) -> str:
-        """
-        Gets the tweet source by id from cache.
-
-        Parameters
-        ----------
-        tweet_id: :class:`int` id of the tweet you're trying to get.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        return self._get_index(tweet_id, "data", "source")
-
-    async def getch_tweet_source(self, tweet_id: int) -> str:
-        """
-        Tries to get the source  of a tweet from cache, if it fails it will make a request to the api.
-
-        Parameters
-        ----------
-        tweet_id: :class:`int` id of the tweet you're trying to get or fetch.
-
-        Returns
-        -------
-        :class:`str`
-        """
-        try:
-            return self._get_index(tweet_id, "data", "source")
-        except KeyError:
-            tweet = await self._request(
-                "GET",
-                f"https://api.twitter.com/2/tweets/{tweet_id}?tweet.fields=source",
-                headers=self.headers,
-            )
-            self._save(tweet_id, tweet)
-            return tweet["data"]["source"]
