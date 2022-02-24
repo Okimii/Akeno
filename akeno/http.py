2bfbdc20e0ecfa63c82c26fefaaeec705cea6d34
Okimii <92546867+Okimii@users.noreply.github.com>
2022-02-23 23:32:17 -0400

    Update http.py

--- akeno/http.py
+++ akeno/http.py
@@ -9,6 +9,7 @@ __all__ = ("HTTPClient",)
 
 class HTTPClient:
     def __init__(self) -> None:
+        self.token = None
         self.lock = asyncio.Lock()
         self.headers = {"Authorization": f"Bearer {self.token}"}
 
@@ -16,15 +17,15 @@ class HTTPClient:
         self, method: str, endpoint: str, headers: dict[Any, Any]
     ) -> dict[Any, Any]:
         self.session = ClientSession(headers=headers)
-        try:
-            await self.lock.acquire()
-            resp = await self.session.request(method, endpoint)
-            if resp.headers["x-rate-limit-remaining"] == 0:
-                await asyncio.sleep(900)
-                self.lock.release()
+        await self.lock.acquire()
+        resp = await self.session.request(method, endpoint)
+        if resp.headers.get("x-rate-limit-remaining") == 0:
+            await asyncio.sleep(900)
             self.lock.release()
-        except KeyError:
+        elif resp.headers.get("x-rate-limit-remaining") is None:
+            self.lock.release()
             await self.session.close()
             return await resp.json()
         await self.session.close()
         return await resp.json()
+        
