2cc62c9342289440ebb100603040d7904ed5392c
CaedenPH <caedenperelliharris@gmail.com>
2022-04-11 21:06:30 +0100

    style: Reformat code and use `__aenter__`

--- akeno/http.py
+++ akeno/http.py
@@ -1,31 +1,41 @@
 from __future__ import annotations
+
 import asyncio
-from typing import Any
 
 from aiohttp import ClientSession
+from typing import Any
+
 
 __all__ = ("HTTPClient",)
 
 
 class HTTPClient:
-    def __init__(self) -> None:
-        self.token = None
+    def __init__(self, token: str) -> None:
+        self.token = token
         self.lock = asyncio.Lock()
-        self.headers = {"Authorization": f"Bearer {self.token}"}
 
-    async def _request(
+    async def request(
         self, method: str, endpoint: str, headers: dict[Any, Any]
     ) -> dict[Any, Any]:
+        """
+        Makes a request to the endpoint
+        with the method and headers.
+
+        Returns
+        -------
+        :class:`dict[Any, Any]`
+            The json returned from the call.
+        """
+
         self.session = ClientSession(headers=headers)
         await self.lock.acquire()
-        resp = await self.session.request(method, endpoint)
-        if resp.headers.get("x-rate-limit-remaining") == 0:
-            await asyncio.sleep(900)
-            self.lock.release()
-        elif resp.headers.get("x-rate-limit-remaining") is None:
-            self.lock.release()
-            await self.session.close()
-            return await resp.json()
-        await self.session.close()
-        return await resp.json()
-        
+
+        async with self.session.request(method, endpoint) as response:
+            if response.headers.get("x-rate-limit-remaining") == 0:
+                await asyncio.sleep(900)
+                self.lock.release()
+            elif response.headers.get("x-rate-limit-remaining") is None:
+                self.lock.release()
+                await self.session.close()
+                return await response.json()
+            return await response.json()
